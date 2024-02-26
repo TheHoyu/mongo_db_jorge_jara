ae8c75b89fb851184dc4f2cd088eda7db3ff51ff
TheHoyu <128851507+TheHoyu@users.noreply.github.com>
2024-02-26 10:54:44 +0100

    Update App.java

--- mongodob/src/main/java/mongodb/mongodob/App.java
+++ mongodob/src/main/java/mongodb/mongodob/App.java
@@ -11,7 +11,7 @@ public class App
     public static void main( String[] args )
     {
         String connectionString="mongodb+srv://GM:1234@cluster0.tbpzes5.mongodb.net/?retryWrites=true&w=majority";
-        
+          //  String connectionString = "mongodb://localhost:27017";
         try(MongoClient mongoClient= MongoClients.create(new ConnectionString(connectionString))){
         	//obtener bbdd 
         	MongoDatabase database = mongoClient.getDatabase("base_mongo");
@@ -36,12 +36,54 @@ public class App
         	}finally {
         		cursor.close();
         	}
-        	
+////////////////////////////////////////////////////////////////////
+            System.out.println("Documentos antes de la modificación:");
+            printAllDocuments(collection);
+
+        // Modificar un documento
+            modificarDocumento(collection, "mis_productos", "edad", 35);
+
+            System.out.println("Documentos después de la modificación:");
+            printAllDocuments(collection);
+
+
+           borrarDocumento(collection, "Ejemplo");
+            
+            System.out.println("Documentos después de la eliminación:");
+            printAllDocuments(collection);
+
+
+            
+            
         }
 
         
-        
-        
+       private static void insertarDocumento(MongoCollection<Document> collection, String nombre, int edad, String ciudad) {
+        Document document = new Document("nombre", nombre)
+                .append("edad", edad)
+                .append("ciudad", ciudad);
+        collection.insertOne(document);
+    }
+
+        private static void modificarDocumento(MongoCollection<Document> collection, String nombre, String campo, Object valor) {
+        collection.updateOne(new Document("nombre", nombre),
+                new Document("$set", new Document(campo, valor)));
+    }
+
+    private static void borrarDocumento(MongoCollection<Document> collection, String nombre) {
+        collection.deleteOne(new Document("nombre", nombre));
+    }
+
+    private static void printAllDocuments(MongoCollection<Document> collection) {
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
+    }
         
     }
 }
